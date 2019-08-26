33c89aa8e0e3469bf1297ad2845d6a51ed8a5bf5
miho1126 <iyq2loveeveryone@gmail.com>
2019-08-26 21:51:58 +0900

    追加
    JSにスクロール追加しました。

--- js/app.js
+++ js/app.js
@@ -7,3 +7,17 @@ $(function () {
     }
   });
 });
+
+$(function () {
+  $('a[href^="#"]').click(function () {
+    var speed = 500;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    //ヘッダーの高さを取得
+    var header = $('header').height() - 50;
+    //ヘッダーの高さを引く
+    var position = target.offset().top - header;
+    $("html, body").animate({ scrollTop: position }, speed, "swing");
+    return false;
+  });
+});
